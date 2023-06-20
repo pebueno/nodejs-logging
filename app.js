000fe688c40c830a685a41a466f23c2093475c34
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2023-06-19 23:52:30 -0300

    Use Winston

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const expressWinston = require('express-winston');
+const { transports, format } = require('winston');
 
 const app = express();
 
+app.use(expressWinston.logger({
+    transports: [
+        new transports.Console()
+    ],
+    format: format.json() // metadata of our request
+}))
 app.get('/', function (req, res) {
-  res.send('Hello World');
+    res.sendStatus(200)
 });
 
 const port = 3000;
