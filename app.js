9f01c80ca0bd41f8100eccc3f7494ba75d1b0b4b
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2023-06-20 00:00:38 -0300

    Generate file of log error

--- app.js
+++ app.js
@@ -4,21 +4,39 @@ const { transports, format } = require('winston');
 
 const app = express();
 
-app.use(expressWinston.logger({
+app.use(
+  expressWinston.logger({
     transports: [
-        new transports.Console()
+        new transports.Console(),
+        new transports.File({
+            level: 'warn',
+            filename: 'logsWarnings.log'
+        }),
+        new transports.File({
+            level: 'error',
+            filename: 'logsErrors.log'
+        }),
     ],
     format: format.combine(
-        format.json(),
-        format.timestamp(),
-        format.prettyPrint()
+      format.json(),
+      format.timestamp(),
+      format.prettyPrint()
+    ),
+    statusLevels: true
+  })
+);
 
-    )
-}))
 app.get('/', function (req, res) {
-    res.sendStatus(200)
+  res.sendStatus(200);
 });
 
+app.get('/400', function (req, res) {
+  res.sendStatus(400);
+});
+
+app.get('/500', function (req, res) {
+  res.sendStatus(500);
+});
 const port = 3000;
 
 app.listen(port, () => {
